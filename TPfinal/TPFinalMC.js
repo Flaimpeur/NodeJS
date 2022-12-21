ba1c692f5ec57e2cadce96b2ab99cd709b7b9284
Flaimpeur <114905674+Flaimpeur@users.noreply.github.com>
2022-12-21 15:17:01 +0100

    Progression : récupération de la bille par le clavier avec la couleur puis fin jeux

--- TPfinal/TPFinalMC.js
+++ TPfinal/TPFinalMC.js
@@ -1,35 +1,37 @@
 // Idéer jeux : masterMind (le jeux ou il y a 5 billes de couleur aléatoire et faut les deviner en 4 essaie)
 
-// Importation du module readline et fs
+// Importation des modules
 const readline = require('readline')
 const fs = require('fs')
+
+// Déclaration de mes variables
+const myBalls = [{B : '🔵', R : '🔴', Y : '🟡', G : '🟢', P : '🟣', O : '🟠', W : '⚪'}]; // billes
+let ballSelec = [] // Billes sélectionner par le joueur
+const interface = readline.createInterface( // Mon interface
+    process.stdin, process.stdout
+); 
+
 
 // Pour faire esthétique, à reprendre plus tard
 const x = (`
 █────█
 `)
 
-const myBalls = [{B : '🔵'},{R : '🔴'},{Y : '🟡'}, {G : '🟢'},{P : '🟣'},{O : '🟠'}, {W : '⚪'}];
-const interface = readline.createInterface(
-    process.stdin, process.stdout);
 
-interface.question('Choose a color between 0 and 6 : ', index =>{
-    console.log(myBalls[index]);
-    interface.close(); // pour fin du jeux
-});
-
-/**
-let objArray= []
-function Obj(array, myProp){
+function Ball(array, myProp){
 
     // Boucle for pour récupérer tout les valeurs des propriétés dans chaque objet
     for (let index = 0; index < array.length; index++) {
-        objArray[index] = array[index][myProp]
+        ballSelec[index] = array[index][myProp]
 
     }
 
     // Renvoie le tableau avec les différente valeur
-    return objArray
+    return ballSelec
 }
 
-console.log(Obj(myBalls, 'B'));*/
+// On demande dans le terminal quel couleur choisir et on affiche la couleur choisi
+interface.question('Choose a color between B, R, Y, G, P, O, W : ', color =>{
+    console.log(Ball(myBalls, color));
+    interface.close(); // pour fin du jeux
+});
