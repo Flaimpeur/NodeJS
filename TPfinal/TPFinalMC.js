9e14f18bc87af11797593a7a102f62d6ade697f4
Flaimpeur <114905674+Flaimpeur@users.noreply.github.com>
2022-12-21 14:34:17 +0100

    Début TPFinal, tableau pour les billes

--- TPfinal/TPFinalMC.js
+++ TPfinal/TPFinalMC.js
@@ -1,2 +1,13 @@
 // Idéer jeux : masterMind (le jeux ou il y a 5 billes de couleur aléatoire et faut les deviner en 4 essaie)
 
+// Importation du module readline
+const readline = require('readline')
+
+// Pour faire esthétique, à reprendre plus tard
+const x = (`
+█────█
+`)
+
+const myBalls = [{B : '🔵'},{R : '🔴'},{Y : '🟡'}, {G : '🟢'},{P : '🟣'},{O : '🟠'}];
+
+console.log(myBalls[0]);
