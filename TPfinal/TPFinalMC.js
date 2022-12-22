b78cda6eab10b148c8ca70073ddb15014fa77389
Flaimpeur <114905674+Flaimpeur@users.noreply.github.com>
2022-12-22 09:57:12 +0100

    Progression : 5 question poser au joueur ET recuperation des 5 couleur dans une variable

--- TPfinal/TPFinalMC.js
+++ TPfinal/TPFinalMC.js
@@ -10,7 +10,8 @@ const { rejects } = require('assert');
 const myBalls = [{B : '🔵', R : '🔴', Y : '🟡', G : '🟢', P : '🟣', O : '🟠', W : '⚪'}]; // billes
 let guessBall = ['B','W','P','R','P']; // Billes sélectionner par l'ordinateur
 let ballSelec = [] // Billes sélectionner par le joueur
-
+let test1 = []
+let test2 = []
 const interface = readline.createInterface( // Mon interface
     process.stdin, process.stdout
 ); 
@@ -46,7 +47,7 @@ const question1 = () =>{
     return new Promise((resolve, rejects)=>{
         interface.question('Choose a first color between B, R, Y, G, P, O, W : ', color1=>{
             console.log(`Your first color is : ${Ball(myBalls, color1)}`);
-            
+            test2 = `${Ball(myBalls, color1)}`
             resolve();
             //interface.close(); // pour fin du jeux
         });
@@ -57,7 +58,7 @@ const question2 = () =>{
     return new Promise((resolve, rejects)=>{
         interface.question('Choose a second color between B, R, Y, G, P, O, W : ', color2=>{
             console.log(`Your second color is : ${Ball(myBalls, color2)}`);
-            
+            test2 = test2 + `${Ball(myBalls, color2)}`
             resolve();
             //interface.close(); // pour fin du jeux
         });
@@ -69,6 +70,7 @@ const question3 = () =>{
         interface.question('Choose a third color between B, R, Y, G, P, O, W : ', color3=>{
             console.log(`Your third color is : ${Ball(myBalls, color3)}`);
             resolve();
+            test2 = test2 + `${Ball(myBalls, color3)}`
             //interface.close(); // pour fin du jeux
         });
     })
@@ -78,6 +80,7 @@ const question4 = () =>{
     return new Promise((resolve, rejects)=>{
         interface.question('Choose a fourth color between B, R, Y, G, P, O, W : ', color4=>{
             console.log(`Your fourth color is : ${Ball(myBalls, color4)}`);
+            test2 = test2 + `${Ball(myBalls, color4)}`
             resolve();
             //interface.close(); // pour fin du jeux
         });
@@ -88,8 +91,9 @@ const question5 = () =>{
     return new Promise((resolve, rejects)=>{
         interface.question('Choose a last color between B, R, Y, G, P, O, W : ', color5=>{
             console.log(`Your last color is : ${Ball(myBalls, color5)}`);
+            test2 = test2 + `${Ball(myBalls, color5)}`
             resolve();
-            
+            console.log(test2)
             //interface.close(); // pour fin du jeux
         });
     })
