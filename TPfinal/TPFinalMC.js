9c06e98ac83ceded32d171b67897037899c2f875
Flaimpeur <114905674+Flaimpeur@users.noreply.github.com>
2022-12-22 12:14:51 +0100

    Progression : nouveau tableau pour récupérer les couleurs en lettre pour comparer

--- TPfinal/TPFinalMC.js
+++ TPfinal/TPFinalMC.js
@@ -1,21 +1,25 @@
-// Idéer jeux : masterMind (le jeux ou il y a 5 billes de couleur aléatoire et faut les deviner en 4 essaie)
-
-// Importation des modules
+//  ----   Importation des modules ----
 const readline = require('readline')
 const fs = require('fs');
 const { resolve } = require('path');
 const { rejects } = require('assert');
 
-// Déclaration de mes variables
+//  ----   Déclaration de mes variables ----
 const myBalls = [{B : '🔵', R : '🔴', Y : '🟡', G : '🟢', P : '🟣', O : '🟠', W : '⚪'}]; // billes
 const botBalls = ['B','R','Y','G','P','O','W'] // Billes mais pour ordi
 let ballSelec = []; // Billes sélectionner par le joueur
-let c1 = []; // Bille 1
-let c2 = []; // Bille 2
-let c3 = []; // Bille 3
-let c4 = []; // Bille 4
-let c5 = []; // Bille 5
-let allcolors = []; // toute les billes
+let cl1 = []; // Bille 1
+let cl2 = []; // Bille 2
+let cl3 = []; // Bille 3
+let cl4 = []; // Bille 4
+let cl5 = []; // Bille 5
+let co1 = []; // Bille 1
+let co2 = []; // Bille 2
+let co3 = []; // Bille 3
+let co4 = []; // Bille 4
+let co5 = []; // Bille 5
+let allLettersColors = []; // toute les billes en lettre qui va juste me servir a vérifier si c'est les bonnes billes
+let allColors = []; // toute les billes en couleur
 const interface = readline.createInterface( // Mon interface
     process.stdin, process.stdout
 );
@@ -42,9 +46,7 @@ const o = (`
 //});
 
 
-
-
-// QUESTION POUR RECUPERATION COULEUR
+//  ----  Fonction pour que le joueur puisse selectionner une billes ----
 
 function Ball(array, myProp){ // Boucle pour pouvoir récupérer les billes dans les questions
 
@@ -58,6 +60,12 @@ function Ball(array, myProp){ // Boucle pour pouvoir récupérer les billes dans
     return ballSelec
 }
 
+
+
+
+//  ----  QUESTION POUR RECUPERATION COULEUR ----
+
+
 const question1 = () =>{
     return new Promise((resolve, rejects)=>{
 
@@ -66,9 +74,10 @@ const question1 = () =>{
             console.log(`Your first color is : ${Ball(myBalls, color1)}`);
 
             // On rentre la couleur dans une nouvelle variable
-            c1 = `${Ball(myBalls, color1)}`
+            cl1 = color1
+            co1 = `${Ball(myBalls, color1)}`
             resolve();
-            //interface.close(); // pour fin du jeux
+
         });
     })
 }
@@ -77,9 +86,10 @@ const question2 = () =>{
     return new Promise((resolve, rejects)=>{
         interface.question('Choose a second color between B, R, Y, G, P, O, W : ', color2=>{
             console.log(`Your second color is : ${Ball(myBalls, color2)}`);
-            c2 = `${Ball(myBalls, color2)}`
+            cl2 = color2
+            co2 = `${Ball(myBalls, color2)}`
             resolve();
-            //interface.close(); // pour fin du jeux
+
         });
     })
 }
@@ -89,8 +99,9 @@ const question3 = () =>{
         interface.question('Choose a third color between B, R, Y, G, P, O, W : ', color3=>{
             console.log(`Your third color is : ${Ball(myBalls, color3)}`);
             resolve();
-            c3 = `${Ball(myBalls, color3)}`
-            //interface.close(); // pour fin du jeux
+            cl3 = color3
+            co3 = `${Ball(myBalls, color3)}`
+
         });
     })
 }
@@ -99,9 +110,10 @@ const question4 = () =>{
     return new Promise((resolve, rejects)=>{
         interface.question('Choose a fourth color between B, R, Y, G, P, O, W : ', color4=>{
             console.log(`Your fourth color is : ${Ball(myBalls, color4)}`);
-            c4 = `${Ball(myBalls, color4)}`
+            cl4 = color4
+            co4 = `${Ball(myBalls, color4)}`
             resolve();
-            //interface.close(); // pour fin du jeux
+
         });
     })
 }
@@ -110,12 +122,13 @@ const question5 = () =>{
     return new Promise((resolve, rejects)=>{
         interface.question('Choose a last color between B, R, Y, G, P, O, W : ', color5=>{
             console.log(`Your last color is : ${Ball(myBalls, color5)}`);
-            c5 = `${Ball(myBalls, color5)}`
+            cl5 = color5
+            co5 = `${Ball(myBalls, color5)}`
             resolve();
             console.log('')
-            allcolors = [c1, c2, c3, c4, c5]
-            console.log(`All your colors are :` + allcolors)
-            //interface.close(); // pour fin du jeux
+            allLettersColors = [cl1, cl2, cl3, cl4, cl5];
+            allColors = [co1, co2, co3, co4, co5];
+            console.log(`All your colors are :` + allColors);
         });
     })
 }
@@ -132,3 +145,4 @@ const myQuestion = async() =>{
 }
 
 myQuestion()
+
